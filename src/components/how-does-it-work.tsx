8d22ef8a118dbbece09500b7f56239b62a738621
Coder-Mehedi <mehedi5051@gmail.com>
2021-09-23 15:19:24 +0600

    step1, step2 done

--- src/components/how-does-it-work.tsx
+++ src/components/how-does-it-work.tsx
@@ -3,10 +3,21 @@ import deskBase from '../primary-desktop.svg';
 import triangle from '../triangle.svg';
 import step1 from '../step1.svg';
 import step2 from '../step2.svg';
+import { useInView } from 'react-intersection-observer';
 
 const HowDoesItWork = () => {
   const { scrollYProgress } = useViewportScroll();
-  const triangleY = useTransform(scrollYProgress, [0, 1], [0, -300]);
+  const triangleY = useTransform(scrollYProgress, [0, 1], [0, -250]);
+  const triangle2Y = useTransform(scrollYProgress, [0, 2], [800, -600]);
+  const triangle3Y = useTransform(scrollYProgress, [0, 1], [6600, 500]);
+
+  const { ref: step2Ref, inView: step2InView } = useInView({
+    threshold: 0.6,
+  });
+
+  const { ref: step3Ref, inView: step3InView } = useInView({
+    threshold: 0.6,
+  });
 
   return (
     <div className='gradientHeaderWithIntro'>
@@ -20,7 +31,18 @@ const HowDoesItWork = () => {
       </div>
       <div className='slideArea'>
         <div className='animationContent'>
-          <motion.div className='baseImgContainer'>
+          <motion.div
+            className='baseImgContainer'
+            animate={
+              step2InView
+                ? {
+                    x: '100%',
+                    y: '100%',
+                  }
+                : { rotateX: 5, rotateY: 10 }
+            }
+            transition={{ duration: 1 }}
+          >
             <div className='imgContainer'>
               <div className='background'>
                 <img src={deskBase} className='base' />
@@ -31,7 +53,7 @@ const HowDoesItWork = () => {
           <div className='stepContent'>
             <motion.img
               src={triangle}
-              className='triangle'
+              className='triangle triangleLeft'
               style={{
                 y: triangleY,
               }}
@@ -47,16 +69,46 @@ const HowDoesItWork = () => {
             </p>
           </div>
 
-          <div className='stepContent step2content'>
-            <h3>Step. 1</h3>
-            <h1>
-              Effortlessly create content structures that flex to your needs
-            </h1>
-            <p>
-              No matter which data structure is the best for your business, you
-              can easily define models and add relations to create rich layout
-              experiences.
-            </p>
+          <div className='stepContent step2' ref={step2Ref}>
+            <div className='step2content'>
+              <h3>Step. 2</h3>
+              <h1>
+                Effortlessly create content structures that flex to your needs
+              </h1>
+              <p>
+                No matter which data structure is the best for your business,
+                you can easily define models and add relations to create rich
+                layout experiences.
+              </p>
+            </div>
+            <motion.img
+              src={triangle}
+              className='triangle triangleRight'
+              style={{
+                y: triangle2Y,
+                rotate: 90,
+              }}
+            />
+          </div>
+          <div className='stepContent step2'>
+            <motion.img
+              src={triangle}
+              className='triangle triangleLeft'
+              // style={{
+              //   y: triangle3Y,
+              // }}
+            />
+            <div className='step3content'>
+              <h3>Step. 1</h3>
+              <h1>
+                Effortlessly create content structures that flex to your needs
+              </h1>
+              <p>
+                No matter which data structure is the best for your business,
+                you can easily define models and add relations to create rich
+                layout experiences.
+              </p>
+            </div>
           </div>
         </div>
       </div>
